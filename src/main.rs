1d43422f2f6a070bf267ae5601914c350f38fbe9
Pi Lanningham <pi.lanningham@gmail.com>
2020-12-02 01:11:08 -0500

    Day 2, Part 2
    Most of this was refactoring for niceness

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ fn solve<P, I, S, R>(file: &str, p: P, s: S) -> Result<R>
 fn main() -> Result<()> {
     println!("1.1) {:?}", solve("input/day1.txt", vec_of::parse, day1::sum_2020)?);
     println!("1.2) {:?}", solve("input/day1.txt", vec_of::parse, day1::sum_3_2020)?);
-    println!("2.1) {:?}", solve("input/day2.txt", vec_of::parse, day2::solve)?);
+    println!("2.1) {:?}", solve("input/day2.txt", vec_of::parse, day2::part1::solve)?);
+    println!("2.2) {:?}", solve("input/day2.txt", vec_of::parse, day2::part2::solve)?);
 
     Ok(())
 }
--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -1,10 +1,10 @@
-use std::str::FromStr;
 use anyhow::*;
+use std::str::FromStr;
 
 #[derive(Debug)]
 pub struct PasswordRecord {
-    min: u64,
-    max: u64,
+    min: usize,
+    max: usize,
     char: char,
     password: String,
 }
@@ -14,7 +14,11 @@ impl FromStr for PasswordRecord {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut parts = s.split(" ");
-        let (range, char, password) = (parts.next().ok_or(())?, parts.next().ok_or(())?, parts.next().ok_or(())?);
+        let (range, char, password) = (
+            parts.next().ok_or(())?,
+            parts.next().ok_or(())?,
+            parts.next().ok_or(())?,
+        );
 
         let mut parts = range.split('-');
         let (min, max) = (parts.next().ok_or(())?, parts.next().ok_or(())?);
@@ -24,37 +28,68 @@ impl FromStr for PasswordRecord {
 
         let password = password.to_string();
 
-        Ok(PasswordRecord { min, max, char, password })
+        Ok(PasswordRecord {
+            min,
+            max,
+            char,
+            password,
+        })
     }
 }
 
-pub fn solve(inputs: Vec<PasswordRecord>) -> Result<u64> {
-    count_valid(&inputs[..])
-}
-
-pub fn count_valid(inputs: &[PasswordRecord]) -> Result<u64> {
+pub fn count_valid<F>(inputs: &[PasswordRecord], v: F) -> Result<u64>
+where
+    F: Fn(&PasswordRecord) -> bool,
+{
     let mut valid = 0;
     for input in inputs {
-        if is_valid(&input) {
+        if v(&input) {
             valid += 1;
         }
     }
     Ok(valid)
 }
 
-pub fn is_valid(record: &PasswordRecord) -> bool {
-    let mut count = 0;
-    for c in record.password.chars() {
-        if c == record.char {
-            count += 1;
-        }
-        if count > record.max {
-            return false;
-        }
+pub mod part1 {
+    use super::*;
+
+    pub fn solve(inputs: Vec<PasswordRecord>) -> Result<u64> {
+        count_valid(&inputs[..], is_valid)
     }
-    return count >= record.min;
+    pub fn is_valid(record: &PasswordRecord) -> bool {
+        let mut count = 0;
+        for c in record.password.chars() {
+            if c == record.char {
+                count += 1;
+            }
+            if count > record.max {
+                return false;
+            }
+        }
+        return count >= record.min;
+    }
 }
 
+pub mod part2 {
+    use super::*;
+    pub fn solve(inputs: Vec<PasswordRecord>) -> Result<u64> {
+        count_valid(&inputs[..], is_valid)
+    }
+    pub fn is_valid(
+        PasswordRecord {
+            min,
+            max,
+            char: c,
+            password,
+        }: &PasswordRecord,
+    ) -> bool {
+        let bytes = password.as_bytes();
+        if *min <= 0 || *max <= 0 || *min > bytes.len() || *max > bytes.len() {
+            return false;
+        }
+        return (bytes[*min - 1] as char == *c) ^ (bytes[*max - 1] as char == *c);
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -66,7 +101,12 @@ mod test {
     fn parse_success() {
         assert_matches!(
             "1-3 a: abcde".parse(),
-            Ok(PasswordRecord { min: 1, max: 3, char: 'a', password: _ })
+            Ok(PasswordRecord {
+                min: 1,
+                max: 3,
+                char: 'a',
+                password: _,
+            })
         );
         assert_eq!(
             "1-3 a: abcde".parse::<PasswordRecord>().unwrap().password,
@@ -74,26 +114,72 @@ mod test {
         );
     }
 
-    #[test]
-    fn valid_success() {
-        assert!(is_valid(&"1-3 a: abcde".parse().unwrap()));
-        assert!(is_valid(&"2-9 c: ccccccccc".parse().unwrap()));
-        assert!(is_valid(&"0-0 x: abcde".parse().unwrap()));
+    mod part1 {
+        use super::super::part1::*;
+        use super::super::*;
+        use matches::assert_matches;
+        
+        #[test]
+        fn valid_success() {
+            assert!(is_valid(&"1-3 a: abcde".parse().unwrap()));
+            assert!(is_valid(&"2-9 c: ccccccccc".parse().unwrap()));
+            assert!(is_valid(&"0-0 x: abcde".parse().unwrap()));
+        }
+
+        #[test]
+        fn valid_failure() {
+            assert_eq!(false, is_valid(&"1-3 b: cdefg".parse().unwrap()));
+            assert_eq!(false, is_valid(&"5-3 b: bbbb".parse().unwrap()));
+            assert_eq!(false, is_valid(&"0-0 b: bb".parse().unwrap()));
+        }
+
+        #[test]
+        fn solve_success() {
+            assert_matches!(
+                count_valid(
+                    &[
+                        "1-3 a: abcde".parse().unwrap(),
+                        "1-3 b: cdefg".parse().unwrap(),
+                        "2-9 c: ccccccccc".parse().unwrap(),
+                    ],
+                    is_valid
+                ),
+                Ok(2)
+            );
+        }
     }
 
-    #[test]
-    fn valid_failure() {
-        assert_eq!(false, is_valid(&"1-3 b: cdefg".parse().unwrap()));
-        assert_eq!(false, is_valid(&"5-3 b: bbbb".parse().unwrap()));
-        assert_eq!(false, is_valid(&"0-0 b: bb".parse().unwrap()));
-    }
+    mod part2 {
+        use super::super::part2::*;
+        use super::super::*;
+        use matches::assert_matches;
+        #[test]
+        fn valid_success() {
+            assert!(is_valid(&"1-3 a: abcde".parse().unwrap()));
+            assert!(is_valid(&"1-3 c: abcde".parse().unwrap()));
+        }
 
-    #[test]
-    fn solve_success() {
-        assert_matches!(count_valid(&[
-            "1-3 a: abcde".parse().unwrap(),
-            "1-3 b: cdefg".parse().unwrap(),
-            "2-9 c: ccccccccc".parse().unwrap(),
-        ]), Ok(2));
+        #[test]
+        fn valid_failure() {
+            assert_eq!(false, is_valid(&"1-3 b: cdefg".parse().unwrap()));
+            assert_eq!(false, is_valid(&"1-3 b: bbbb".parse().unwrap()));
+            assert_eq!(false, is_valid(&"0-0 b: bb".parse().unwrap()));
+            assert_eq!(false, is_valid(&"10-0 b: bb".parse().unwrap()));
+        }
+
+        #[test]
+        fn solve_success() {
+            assert_matches!(
+                count_valid(
+                    &[
+                        "1-3 a: abcde".parse().unwrap(),
+                        "1-3 b: cdefg".parse().unwrap(),
+                        "2-9 c: ccccccccc".parse().unwrap(),
+                    ],
+                    is_valid
+                ),
+                Ok(1)
+            );
+        }
     }
 }
