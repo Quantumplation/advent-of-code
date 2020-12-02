7d4c8132db8eedd07414151a70e627757150e514
Pi Lanningham <pi.lanningham@gmail.com>
2020-12-02 00:43:55 -0500

    Combine Day 1 parts into one file
    With 25 days ahead, and lots of likely code reuse, it's probably better to combine them

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,8 +1,27 @@
-use super::day1_1::find_product;
 use anyhow::*;
+use std::{collections::HashSet};
+
+pub fn sum_2020(numbers: Vec<u64>) -> Result<u64> {
+    find_product(2020, &numbers[..])
+}
 
 pub fn sum_3_2020(numbers: Vec<u64>) -> Result<u64> {
     find_3_product(2020, &numbers[..])
+}
+
+pub fn find_product(sum: u64, numbers: &[u64]) -> Result<u64> {
+    let mut seen = HashSet::new();
+    for &n in numbers {
+        if n > sum {
+            continue;
+        }
+        let counterpart = sum - n;
+        if seen.contains(&counterpart) {
+            return Ok(n * counterpart);
+        }
+        seen.insert(n);
+    }
+    bail!("No numbers sum to 2020");
 }
 
 pub fn find_3_product(sum: u64, numbers: &[u64]) -> Result<u64> {
@@ -20,19 +39,32 @@ pub fn find_3_product(sum: u64, numbers: &[u64]) -> Result<u64> {
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
     use matches::assert_matches;
 
     #[test]
-    fn success() {
+    fn part1_success() {
+        assert_matches!(find_product(2020, &[2019, 1]), Ok(2019));
+        assert_matches!(find_product(2020, &[2015, 5, 3]), Ok(10075));
+        assert_matches!(find_product(2020, &[1721,979,366,299,675,1456]), Ok(514579));
+    }
+
+    #[test]
+    fn part1_failure() {
+        assert_matches!(find_product(2020, &[10, 11]), Err(_));
+    }
+
+
+    #[test]
+    fn part2_success() {
         assert_matches!(find_3_product(2020, &[2018, 1, 1]), Ok(2018));
         assert_matches!(find_3_product(2020, &[2015, 2, 3, 7]), Ok(12090));
         assert_matches!(find_3_product(2020, &[1721,979,366,299,675,1456]), Ok(241861950));
     }
 
     #[test]
-    fn failure() {
+    fn part2_failure() {
         assert_matches!(find_3_product(2020, &[1,2]), Err(_));
         assert_matches!(find_3_product(2020, &[1,2,3]), Err(_));
     }
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,2 +1,1 @@
-pub mod day1_1;
-pub mod day1_2;
+pub mod day1;
