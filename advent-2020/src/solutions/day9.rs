48d9ab7241018fe351ab6fd2b9312fd6159919fd
Pi Lanningham <pi.lanningham@gmail.com>
2020-12-09 00:42:55 -0500

    Year 2020, Day 9, Part 2

--- advent-2020/src/solutions/day9.rs
+++ advent-2020/src/solutions/day9.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::{cmp::Ordering, collections::{HashMap, HashSet, VecDeque}};
 
 use anyhow::*;
 
@@ -6,6 +6,24 @@ pub mod part1 {
   use super::*;
   pub fn solve(numbers: Vec<u64>) -> Result<u64> {
     Ok(find_first_invalid_number(25, &numbers))
+  }
+}
+pub mod part2 {
+  use super::*;
+  pub fn solve(numbers: Vec<u64>) -> Result<u64> {
+    let target = find_first_invalid_number(25, &numbers);
+    let (lower, upper) = find_contiguous_sum(target, &numbers);
+    let mut min = u64::MAX;
+    let mut max = 0;
+    for number in numbers[lower..upper].iter() {
+      if *number < min {
+        min = *number;
+      }
+      if *number > max {
+        max = *number;
+      }
+    }
+    return Ok(min + max);
   }
 }
 
@@ -31,35 +49,52 @@ pub fn find_first_invalid_number(trail: usize, numbers: &Vec<u64>) -> u64 {
   panic!();
 }
 
+pub fn find_contiguous_sum(target: u64, numbers: &Vec<u64>) -> (usize, usize) {
+  let mut front = 0;
+  let mut back = 0;
+  let mut sum: u64 = 0;
+  loop {
+    if front > numbers.len() && sum != target {
+      break;
+    }
+    match sum.cmp(&target) {
+      Ordering::Less => {
+        sum += numbers[front];
+        front += 1;
+      },
+      Ordering::Greater => {
+        sum -= numbers[back];
+        back += 1;
+      },
+      Ordering::Equal => {
+        return (back, front);
+      },
+    }
+  }
+  panic!("Unable to find a consecutive sum");
+}
+
 #[cfg(test)]
 mod tests {
-  use matches::assert_matches;
   use super::*;
 
   #[test]
-  fn success() {
+  fn find_invalid_number() {
     let numbers = vec![
-      35,
-      20,
-      15,
-      25,
-      47,
-      40,
-      62,
-      55,
-      65,
-      95,
-      102,
-      117,
-      150,
-      182,
-      127,
-      219,
-      299,
-      277,
-      309,
-      576
+      35, 20, 15, 25, 47, 40, 62,
+      55, 65, 95, 102, 117, 150, 182,
+      127, 219, 299, 277, 309, 576
     ];
     assert_eq!(127, find_first_invalid_number(5, &numbers));
+  }
+
+  #[test]
+  fn find_consecutive_sum() {
+    let numbers = vec![
+      35, 20, 15, 25, 47, 40, 62,
+      55, 65, 95, 102, 117, 150, 182,
+      127, 219, 299, 277, 309, 576
+    ];
+    assert_eq!((2,6), find_contiguous_sum(127, &numbers));
   }
 }
