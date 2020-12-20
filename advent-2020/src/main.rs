10dbb5a0d1c6bb31c66ef41245e199f538b13a4d
Pi Lanningham <pi.lanningham@gmail.com>
2020-12-19 21:24:12 -0500

    Year 2020, Day 19, Part 2

--- advent-2020/src/main.rs
+++ advent-2020/src/main.rs
@@ -8,6 +8,7 @@ use anyhow::*;
 
 fn main() -> Result<()> {
     println!("19.1) {:?}", run(2020, "day19.txt", identity,         day19::part1::solve));
+    println!("19.2) {:?}", run(2020, "day19.txt", identity,         day19::part2::solve));
     return Ok(());
     println!("1.1) {:?}", run(2020, "day1.txt", vec_of,             day1::part1::solve));
     println!("1.2) {:?}", run(2020, "day1.txt", vec_of,             day1::part2::solve));
--- advent-2020/src/solutions/day19.rs
+++ advent-2020/src/solutions/day19.rs
@@ -1,11 +1,38 @@
-use std::{collections::HashMap, str::FromStr};
+use std::iter::FromIterator;
+use std::{
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 pub mod part1 {
-  use super::*;
-  use anyhow::*;
-  pub fn solve(input: Input) -> Result<usize> {
-    return Ok(input.count_satisfying());
-  }
+    use super::*;
+    use anyhow::*;
+    pub fn solve(input: Input) -> Result<usize> {
+        return Ok(input.count_satisfying());
+    }
+}
+pub mod part2 {
+    use super::*;
+    use anyhow::*;
+    pub fn solve(input: Input) -> Result<usize> {
+        let mut input = input;
+        input.rules.insert(
+            8,
+            RulePart::Or(vec![
+                RulePart::Sequence(vec![42]),
+                RulePart::Sequence(vec![42, 8]),
+            ]),
+        );
+
+        input.rules.insert(
+          11,
+          RulePart::Or(vec![
+              RulePart::Sequence(vec![42, 31]),
+              RulePart::Sequence(vec![42, 11, 31]),
+          ]),
+      );
+        return Ok(input.count_satisfying());
+    }
 }
 
 #[derive(Debug)]
@@ -22,7 +49,7 @@ pub struct Rule {
 }
 
 pub struct Input {
-    rules: HashMap<u32, Rule>,
+    rules: HashMap<u32, RulePart>,
     messages: Vec<String>,
 }
 
@@ -33,10 +60,20 @@ impl FromStr for RulePart {
             return Ok(RulePart::Literal(s.chars().nth(1).unwrap()));
         }
         if s.contains("|") {
-            return Ok(RulePart::Or(s.split("|").filter(|&s| !s.is_empty()).map(|s| s.trim().parse::<RulePart>().unwrap()).collect()))
+            return Ok(RulePart::Or(
+                s.split("|")
+                    .filter(|&s| !s.is_empty())
+                    .map(|s| s.trim().parse::<RulePart>().unwrap())
+                    .collect(),
+            ));
         }
 
-        Ok(RulePart::Sequence(s.split(" ").filter(|&s| !s.is_empty()).map(|s| s.trim().parse::<u32>().unwrap()).collect()))
+        Ok(RulePart::Sequence(
+            s.split(" ")
+                .filter(|&s| !s.is_empty())
+                .map(|s| s.trim().parse::<u32>().unwrap())
+                .collect(),
+        ))
     }
 }
 
@@ -46,7 +83,7 @@ impl FromStr for Rule {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut split = s.split(":");
         let id = split.next().unwrap().trim().parse::<u32>().unwrap();
-        
+
         let rest = split.next().unwrap().trim();
         let rules = rest.parse::<RulePart>().unwrap();
         return Ok(Rule { id, rules });
@@ -62,8 +99,8 @@ impl FromStr for Input {
         for line in s.lines() {
             if line.contains(":") {
                 let rule = line.trim().parse::<Rule>().unwrap();
-                rules.insert(rule.id, rule);
-            } else {
+                rules.insert(rule.id, rule.rules);
+            } else if !line.trim().is_empty() {
                 messages.push(line.trim().to_string());
             }
         }
@@ -72,29 +109,41 @@ impl FromStr for Input {
 }
 
 impl RulePart {
-    pub fn satisfies(&self, input: &str, rules: &HashMap<u32, Rule>) -> (bool, usize) {
+    pub fn satisfies(
+        &self,
+        input: &str,
+        rules: &HashMap<u32, RulePart>,
+        depth: usize,
+    ) -> HashSet<usize> {
         match &self {
-            &RulePart::Literal(c) => (input.starts_with(*c), 1),
-            &RulePart::Sequence(parts) => {
-                let mut eaten = 0;
-                for part in parts {
-                    let rule = rules.get(&part).unwrap();
-                    let (matches, eats) = rule.rules.satisfies(&input[eaten..], rules);
-                    if !matches {
-                        return (false, 0);
-                    }
-                    eaten += eats;
+            &RulePart::Literal(c) => {
+                if input.starts_with(*c) {
+                    HashSet::from_iter(vec![1])
+                } else {
+                    HashSet::new()
                 }
-                return (true, eaten);
+            }
+            &RulePart::Sequence(parts) => {
+                let mut reachable_offsets = vec![HashSet::from_iter(vec![0])];
+                for (idx, part) in parts.iter().enumerate() {
+                    if reachable_offsets[idx].len() == 0 {
+                        return HashSet::new();
+                    }
+                    reachable_offsets.push(HashSet::new());
+                    let rule = rules.get(&part).unwrap();
+                    for offset in reachable_offsets[idx].clone() {
+                        let sats = rule.satisfies(&input[offset..], rules, depth + 1);
+                        reachable_offsets[idx + 1].extend(sats.iter().map(|s| s + offset));
+                    }
+                }
+                return reachable_offsets[parts.len()].clone();
             }
             &RulePart::Or(options) => {
+                let mut hs = HashSet::new();
                 for option in options {
-                    let (matches, eats) = option.satisfies(input, rules);
-                    if matches {
-                        return (matches, eats);
-                    }
+                    hs.extend(option.satisfies(input, rules, depth + 1));
                 }
-                return (false, 0);
+                return hs;
             }
         }
     }
@@ -103,11 +152,15 @@ impl RulePart {
 impl Input {
     pub fn satisfies(&self, input: &str) -> bool {
         let root = self.rules.get(&0).unwrap();
-        let (matches, eaten) = root.rules.satisfies(input, &self.rules);
-        return matches && eaten == input.len();
+        let reachable_offsets = root.satisfies(input, &self.rules, 0);
+        return reachable_offsets.contains(&input.len());
     }
     pub fn count_satisfying(&self) -> usize {
-        self.messages.iter().filter(|&s| self.satisfies(s)).count()
+        let mut total = 0;
+        for m in self.messages.iter().filter(|&s| self.satisfies(s)) {
+            total += 1;
+        }
+        return total;
     }
 }
 
@@ -121,7 +174,7 @@ mod tests {
         assert_matches!("\"a\"".parse::<RulePart>(), Ok(RulePart::Literal('a')));
         let simple = "1 2".parse::<RulePart>();
         if let Ok(RulePart::Sequence(pieces)) = simple {
-            assert_eq!(vec![1,2], pieces);
+            assert_eq!(vec![1, 2], pieces);
         } else {
             panic!();
         }
@@ -129,8 +182,16 @@ mod tests {
         if let Ok(RulePart::Or(pieces)) = or {
             assert_matches!(pieces[0], RulePart::Sequence(_));
             assert_matches!(pieces[1], RulePart::Sequence(_));
-            let left = if let RulePart::Sequence(left) = &pieces[0] { left } else { panic!() };
-            let right = if let RulePart::Sequence(left) = &pieces[1] { left } else { panic!() };
+            let left = if let RulePart::Sequence(left) = &pieces[0] {
+                left
+            } else {
+                panic!()
+            };
+            let right = if let RulePart::Sequence(left) = &pieces[1] {
+                left
+            } else {
+                panic!()
+            };
             assert_eq!(1, left[0]);
             assert_eq!(2, left[1]);
             assert_eq!(2, right[0]);
@@ -157,14 +218,83 @@ mod tests {
                           aaaabbb";
         let input = input.parse::<Input>().unwrap();
         let simple = input.rules.get(&4).unwrap();
-        assert_eq!((true, 1), simple.rules.satisfies("a", &input.rules));
-        assert_eq!((false, 1), simple.rules.satisfies("b", &input.rules));
-        let complex = input.rules.get(&2).unwrap();
-        assert_eq!((true, 2), complex.rules.satisfies("aa", &input.rules));
-        assert_eq!((true, 2), complex.rules.satisfies("bb", &input.rules));
+        // assert_eq!((true, 1), simple.rules.satisfies("a", &input.rules, 0));
+        // assert_eq!((false, 1), simple.rules.satisfies("b", &input.rules, 0));
+        // let complex = input.rules.get(&2).unwrap();
+        // assert_eq!((true, 2), complex.rules.satisfies("aa", &input.rules, 0));
+        // assert_eq!((true, 2), complex.rules.satisfies("bb", &input.rules, 0));
         assert!(input.satisfies("aaaabb"));
         assert_eq!(false, input.satisfies("baaabb"));
 
         assert_eq!(2, input.count_satisfying());
+    }
+
+    #[test]
+    fn mo_example() {
+        let input = "0: 1 2
+         1: 4 | 4 1
+         2: 4 3 | 4 2 3
+         3: \"a\"
+         4: \"b\"
+         ba
+         bba
+         bbbaa
+         bbbbbbbbbbbba
+        ";
+        let input = input.parse::<Input>().unwrap();
+        assert_eq!(3, input.count_satisfying());
+    }
+
+    #[test]
+    fn large_example() {
+        let input = "42: 9 14 | 10 1
+        9: 14 27 | 1 26
+        10: 23 14 | 28 1
+        1: \"a\"
+        11: 42 31 | 42 11 31
+        5: 1 14 | 15 1
+        19: 14 1 | 14 14
+        12: 24 14 | 19 1
+        16: 15 1 | 14 14
+        31: 14 17 | 1 13
+        6: 14 14 | 1 14
+        2: 1 24 | 14 4
+        0: 8 11
+        13: 14 3 | 1 12
+        15: 1 | 14
+        17: 14 2 | 1 7
+        23: 25 1 | 22 14
+        28: 16 1
+        4: 1 1
+        20: 14 14 | 1 15
+        3: 5 14 | 16 1
+        27: 1 6 | 14 18
+        14: \"b\"
+        21: 14 1 | 1 14
+        25: 1 1 | 1 14
+        22: 14 14
+        8: 42 | 42 8
+        26: 14 22 | 1 20
+        18: 15 15
+        7: 14 5 | 1 21
+        24: 14 1
+
+        abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+        bbabbbbaabaabba
+        babbbbaabbbbbabbbbbbaabaaabaaa
+        aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+        bbbbbbbaaaabbbbaaabbabaaa
+        bbbababbbbaaaaaaaabbababaaababaabab
+        ababaaaaaabaaab
+        ababaaaaabbbaba
+        baabbaaaabbaaaababbaababb
+        abbbbabbbbaaaababbbbbbaaaababb
+        aaaaabbaabaaaaababaa
+        aaaabbaaaabbaaa
+        aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+        babaaabbbaaabaababbaabababaaab
+        aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba";
+        let input = input.parse::<Input>().unwrap();
+        assert_eq!(12, input.count_satisfying());
     }
 }
